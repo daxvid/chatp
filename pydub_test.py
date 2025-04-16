98cf5b9d7fa08419258459f51c5cee08c9b8f1e6
dav <daxvid32@gmail.com>
2025-04-16 16:27:10 +0800

    Create pydub_test.py

---
+++ pydub_test.py
@@ -0,0 +1,18 @@
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+from datetime import datetime
+
+start_time = datetime.now()
+audio = AudioSegment.from_wav("recordings/input.wav")
+chunks = split_on_silence(audio, 
+    min_silence_len=1000,
+    silence_thresh=-50,
+    keep_silence=200
+)
+
+for i, chunk in enumerate(chunks):
+    chunk.export(f"recordings/output_{i:03d}.wav", format="wav")
+
+end_time =  datetime.now()
+print(f"Total chunks: {len(chunks)}")
+print(f"Time taken: {end_time - start_time}")

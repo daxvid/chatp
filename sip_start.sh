73f301e324e4140e60045ddbcc19bbcb1c6d8c4c
dav <daxvid32@gmail.com>
2025-05-29 17:09:39 +0800

    Update sip_start.sh

--- sip_start.sh
+++ sip_start.sh
@@ -22,6 +22,14 @@ while true; do
     for config in 967 968 969 970 971 972 973 974 975 976 977 978 979 980 981 982 983 984 985 986 987 988 989 990 991 992 993 994 995 996 997 998 999 158 159 160 161 162 163 164 165 166 167 168 169 200 201 202 288 290; do
         if ! pgrep -f "python3 main.py ./conf/config${config}.yaml" > /dev/null; then
             echo "$(date '+%Y-%m-%d %H:%M:%S') config${config}.yaml 正在启动..." >> "$LOG_FILE"
+            #检查config${config}.yaml是否存在
+            if [ ! -f ./conf/config${config}.yaml ]; then
+                continue
+            fi
+            #tel${config}.txt是否存在
+            if [ ! -f ./conf/tel${config}.txt ]; then
+                continue
+            fi
             nohup python3 main.py ./conf/config${config}.yaml > ${config}.log 2>&1 &
             sleep 3
         fi
